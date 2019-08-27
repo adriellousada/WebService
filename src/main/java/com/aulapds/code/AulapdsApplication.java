561bb0401beb8bc4c1e1a162e495b8bae260d3bb
Adriel Lousada <adriellousada@gmail.com>
2019-08-27 20:14:10 -0300

    Project Created

---
+++ src/main/java/com/aulapds/code/AulapdsApplication.java
@@ -0,0 +1,13 @@
+package com.aulapds.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulapdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulapdsApplication.class, args);
+	}
+
+}
