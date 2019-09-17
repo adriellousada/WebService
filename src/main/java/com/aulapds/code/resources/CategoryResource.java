6bce45211ec86a2da78c478b707981b699bd0347
Adriel Lousada <adriellousada@gmail.com>
2019-09-17 20:33:37 -0300

    Challenge: category CRUD

--- src/main/java/com/aulapds/code/resources/CategoryResource.java
+++ src/main/java/com/aulapds/code/resources/CategoryResource.java
@@ -18,41 +18,41 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.aulapds.code.entities.Category;
 import com.aulapds.code.services.CategoryService;
 
-
 @RestController
-@RequestMapping(value = "/categories")
+@RequestMapping(value ="/categories")
 public class CategoryResource {
 	
 	@Autowired
 	private CategoryService service;
-	
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<Category> list= service.findAll();
 		
+		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Category> findById(@PathVariable Long id){
-		Category obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
-		
-	}
 	
+		Category obj = service.findById(id);
+
+		return ResponseEntity.ok().body(obj) ;
+	}
+
 	@PostMapping
 	public ResponseEntity<Category> insert(@RequestBody Category obj){
 		obj = service.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).body(obj);
+		URI uri= ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj) ;
 	}
-	
+
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category obj){
 		obj = service.update(id, obj);
--- src/main/java/com/aulapds/code/services/CategoryService.java
+++ src/main/java/com/aulapds/code/services/CategoryService.java
@@ -3,42 +3,60 @@ package com.aulapds.code.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.aulapds.code.entities.Category;
 import com.aulapds.code.repositories.CategoryRepository;
+import com.aulapds.code.services.exceptions.DatabaseException;
+import com.aulapds.code.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
-	public List<Category> findAll(){
+
+	public List<Category> findAll() {
+
 		return repository.findAll();
 	}
-	
+
 	public Category findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
-	
+
 	public Category insert(Category obj) {
 		return repository.save(obj);
 	}
-	
-	public void delete (Long id) {
-		repository.deleteById(id);
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+			}catch (EmptyResultDataAccessException e) {
+				throw new ResourceNotFoundException(id);
+			}catch (DataIntegrityViolationException e){
+				throw new DatabaseException(e.getMessage());
+				}
 	}
-	
+
 	public Category update(Long id, Category obj) {
-		Category entity =  repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			Category entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+			} catch (EntityNotFoundException e) {
+				throw new ResourceNotFoundException(id);
+			}
 	}
 
 	private void updateData(Category entity, Category obj) {
 		entity.setName(obj.getName());
+
 	}
 }
