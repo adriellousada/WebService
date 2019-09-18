f96bf5b32e1c864c6f7c6baf81b4c11a1d3e815e
Adriel Lousada <adriellousada@gmail.com>
2019-09-17 21:12:04 -0300

    User update, @Transactional

--- src/main/java/com/aulapds/code/services/UserService.java
+++ src/main/java/com/aulapds/code/services/UserService.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.transaction.Transactional;
 import javax.persistence.EntityNotFoundException;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -48,21 +50,21 @@ public class UserService {
 			throw new DatabaseException(e.getMessage());
 			}
 	}
-	
-	public User update(Long id, User obj) {
+	@Transactional
+	public UserDTO update(Long id, UserDTO dto) {
 		try {
 		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		updateData(entity, dto);
+		entity = repository.save(entity);
+		return new UserDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException(id);
 		}
 	}
 
-	private void updateData(User entity, User obj) {
-		entity.setName(obj.getName());
-		entity.setEmail(obj.getEmail());
-		entity.setPhone(obj.getPhone());
-		
+	private void updateData(User entity, UserDTO dto) {
+		entity.setName(dto.getName());
+		entity.setEmail(dto.getEmail());
+		entity.setPhone(dto.getPhone());
 	}
 }
