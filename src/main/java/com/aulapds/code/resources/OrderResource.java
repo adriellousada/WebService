788aea662072ef259751ff06a589304c8bccb0dd
Adriel Lousada <adriellousada@gmail.com>
2019-10-07 21:16:35 -0300

    OrderDTO

--- src/main/java/com/aulapds/code/resources/OrderResource.java
+++ src/main/java/com/aulapds/code/resources/OrderResource.java
@@ -9,29 +9,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.aulapds.code.entities.Order;
+import com.aulapds.code.dto.OrderDTO;
 import com.aulapds.code.services.OrderService;
 
-
 @RestController
-@RequestMapping(value = "/orders")
+@RequestMapping(value ="/orders")
 public class OrderResource {
 	
 	@Autowired
 	private OrderService service;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Order>> findAll(){
-		List<Order> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+	public ResponseEntity<List<OrderDTO>> findAll(){
+		List<OrderDTO> list= service.findAll();
 		
+		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Order> findById(@PathVariable Long id){
-		Order obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
-		
-	}
+	public ResponseEntity<OrderDTO> findById(@PathVariable Long id){
 	
+		OrderDTO dto = service.findById(id);
+		
+		return ResponseEntity.ok().body(dto) ;
+	}
 }
--- src/main/java/com/aulapds/code/services/OrderService.java
+++ src/main/java/com/aulapds/code/services/OrderService.java
@@ -2,25 +2,32 @@ package com.aulapds.code.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.aulapds.code.dto.OrderDTO;
 import com.aulapds.code.entities.Order;
 import com.aulapds.code.repositories.OrderRepository;
+import com.aulapds.code.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class OrderService {
-	
+
 	@Autowired
 	private OrderRepository repository;
 	
-	public List<Order> findAll(){
-		return repository.findAll();
+	public List<OrderDTO> findAll() {
+
+		List<Order>list = repository.findAll();
+		return list.stream().map(e -> new OrderDTO(e)).collect(Collectors.toList());
 	}
-	
-	public Order findById(Long id) {
+
+	public OrderDTO findById(Long id) {
 		Optional<Order> obj = repository.findById(id);
-		return obj.get();
+		Order entity= obj.orElseThrow(()-> new ResourceNotFoundException(id));
+		return new OrderDTO(entity);
 	}
+
 }
